58d93321b8c3c56bb7bf7fcbfefe93221ace4051
Gabrielrpg68 <rm88737@fiap.com.br>
2021-09-22 17:46:51 -0300

    Importado Cabecalho e Rodape para o App

--- sorveteria/src/App.js
+++ sorveteria/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Corpo from './componentes/corpo/Corpo'
 import styled from 'styled-components'
-
+import Rodape from './componentes/rodape/Rodape'
+import Cabecalho from './componentes/cabecalho/Cabecalho'
 
 export default function App() {
 
    return(
       <>
-      <Corpo/>
+         <Cabecalho/>
+         <Corpo/>
+         <Rodape/>
       </>
 )}
