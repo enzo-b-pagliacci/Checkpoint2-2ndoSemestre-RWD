0643f616b83f6aa239aeb5b71a3944968543fa50
Gustavo-Lopes-Carlin-fiap <RM87339@fiap.com.br>
2021-09-22 17:37:29 -0300

    comecando a personalizar corpo

--- sorveteria/src/componentes/corpo/Corpo.js
+++ sorveteria/src/componentes/corpo/Corpo.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+export const H2 = styled.h2`
+font-size: 16px;
+color: blue;
+`
+export const Ul = styled.ul`
+color: green;
+`
 
 export default function Corpo() {
 
    return(
       <>
-      <h2></h2>
-      <ul>
+      <H2>Sabores</H2>
+      <Ul>
+          <li>Uva</li>
+          <li>Limão</li>
+          <li>Morango</li>
           <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-      </ul>
+      </Ul>
       <h2></h2>
       <ul>
           <li></li>
