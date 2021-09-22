f8052d3d885c980ff31af9396618a3f3b28ccb29
Jhoctan21 <rm88206@fiap.com.br>
2021-09-22 20:11:53 -0300

    Alterando cabecalho

--- sorveteria/src/componentes/cabecalho/Cabecalho.js
+++ sorveteria/src/componentes/cabecalho/Cabecalho.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Logo from '../img/sorveteria.png'
+// import styled from 'styled-components'
 
+
+
+// export const IMG = styled.img`
+// width: 7%;
+// `
+// export const HEADER = styled.header`
+// background-color: blue;
+// `
 export default function Cabecalho () {
+
 
    return(
        <>
-            <h1>Sorveteria IceLand</h1>
-            <p>Keep calm and let it go</p>
+          <HEADER>
+               <IMG src={Logo} alt="SORVETERIA ICE CREAM" />
+               <h1>Sorveteria IceLand</h1>
+               <p>Keep calm and let it go</p>
+          </HEADER>
        </>
 )}
--- sorveteria/styled.js
+++ sorveteria/styled.js
@@ -1,1 +1,8 @@
 import styled from 'styled-components'
+
+export const IMG = styled.img`
+width: 7%;
+`
+export const HEADER = styled.header`
+background-color: blue;
+`
