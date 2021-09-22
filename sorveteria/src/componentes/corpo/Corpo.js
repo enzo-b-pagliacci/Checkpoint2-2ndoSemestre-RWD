adb2ffcac3943969151521e095936584f633f450
Gustavo-Lopes-Carlin-fiap <RM87339@fiap.com.br>
2021-09-22 16:26:44 -0300

    corpo

---
+++ sorveteria/src/componentes/corpo/Corpo.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Corpo() {
+
+   return(
+      <>
+      <h2></h2>
+      <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+      </ul>
+      <h2></h2>
+      <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+      </ul>
+      </>
+)}
