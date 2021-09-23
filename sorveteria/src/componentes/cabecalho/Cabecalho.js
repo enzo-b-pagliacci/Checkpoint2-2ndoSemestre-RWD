446035e694e623b12a5f80841aedc7522329bea9
Jhoctan21 <rm88206@fiap.com.br>
2021-09-23 16:08:36 -0300

    Estilo cabecalho

--- sorveteria/src/componentes/cabecalho/Cabecalho.js
+++ sorveteria/src/componentes/cabecalho/Cabecalho.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import {DivCabe, Text, TituloIce} from '../../styled'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faIceCream } from '@fortawesome/free-solid-svg-icons'
+import {DivCabe, Text, TituloIce, DivTudo} from '../../styled'
+
 export default function Cabecalho () {
 
 
    return(
        <>
-       <div style={{backgroundColor: '#f28d11'}}>
-            <DivCabe>                   
-                  <TituloIce><FontAwesomeIcon style={{color: 'white', fontSize:'40px'}}icon={faIceCream} />   Sorveteria IceLand</TituloIce>    
-            </DivCabe>
-            <div style={{textAlign: 'center'}}>       
-               <Text>Keep calm and let it go</Text>
-            </div>
-         </div>
-         
+            <DivTudo>
+                  <DivCabe>                         
+                        <TituloIce>Sorveteria IceLand</TituloIce> 
+                        <Text>Keep calm and let it go</Text>   
+                  </DivCabe> 
+            </DivTudo>
        </>
 )}
--- sorveteria/src/styled.js
+++ sorveteria/src/styled.js
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 
-export const DivCabe = styled.div `
+export const DivTudo = styled.div`
 @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
 font-family:  'Lobster', cursive;
-font-size: 1.4em;
+background: rgb(160,239,255);
+background: linear-gradient(0deg, rgba(160,239,255,1) 0%, rgba(45,70,253,1) 60%);
 `
- export const TituloIce = styled.h1`
- text-align: center;
+export const DivCabe = styled.div `
+text-align: center;
+`
+export const TituloIce = styled.h1`
+color: #aeaeff;
+font-size: 3em;
+padding-top: 25px;
  `
 export const Text = styled.p `
-font-family:  'Lobster', cursive;
-color: Yellow;
-font-size: 1.2em;
-padding-bottom: 10px;
+color: #fff;
+font-size: 1.4em;
+padding-bottom: 1em;
 `
